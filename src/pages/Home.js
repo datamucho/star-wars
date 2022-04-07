ea64d12f70a6522294f62af2d35000d5423f420e
datamucho <74556987+datamucho@users.noreply.github.com>
2022-04-08 02:37:18 +0400

    Add files via upload

---
+++ src/pages/Home.js
@@ -0,0 +1,32 @@
+import React, { useState } from "react";
+import Pagination from "../components/Pagination";
+import Search from "../components/Search";
+import CharacterList from "../components/CharacterList";
+import useFetch from "../api/useFetch";
+
+const Home = () => {
+  const [pageIndex, setPageIndex] = useState(1);
+  const { data, loading, error } = useFetch(
+    `https://swapi.dev/api/people/?page=${pageIndex}`
+  );
+
+  if (loading) {
+    return <h1 style={{ color: "#fff" }}>loading...</h1>;
+  }
+
+  return (
+    <div className="hp">
+      <Search data={data} />
+
+      <CharacterList data={data} />
+
+      <Pagination
+        next={() => setPageIndex((prevState) => prevState + 1)}
+        id={pageIndex}
+        back={() => setPageIndex((prevState) => prevState - 1)}
+      />
+    </div>
+  );
+};
+
+export default Home;
