9331e51f016ec522755191a70ec9868cac0d2699
datamucho <74556987+datamucho@users.noreply.github.com>
2022-04-08 14:49:18 +0400

    Add files via upload

---
+++ src/components/Pagination.js
@@ -0,0 +1,33 @@
+import React from "react";
+
+const Pagination = ({ next, back, id, setPage }) => {
+  const pages = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+
+  return (
+    <div className="pagination">
+      {id > 1 && (
+        <button onClick={() => setPage(id - 1)} className="btn">
+          Back
+        </button>
+      )}
+      {pages.map((item) =>
+        item === id ? (
+          <button className="btn-curr" onClick={() => setPage(item)}>
+            {item}
+          </button>
+        ) : (
+          <button className="btn" onClick={() => setPage(item)}>
+            {item}
+          </button>
+        )
+      )}
+      {id < 9 && (
+        <button onClick={() => setPage(id + 1)} className="btn">
+          Next
+        </button>
+      )}
+    </div>
+  );
+};
+
+export default Pagination;
